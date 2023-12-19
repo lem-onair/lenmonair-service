d7aa43cc92e98ed59c482b37b3ac2329aad67496
BYEONGRYEOL <lyulbyung@gmail.com>
2023-12-19 22:16:53 +0900

    홈페이지에서 따로 방송 시작 종료 요청을 보내지 않도록 수정..

--- src/main/java/com/hanghae/lemonairservice/controller/StreamController.java
+++ src/main/java/com/hanghae/lemonairservice/controller/StreamController.java
@@ -20,34 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class StreamController {
     private final StreamService streamService;
 
-    @PostMapping("/{streamerId}/check")
-    public ResponseEntity<Boolean> checkStreamValidity(@PathVariable String streamerId, @RequestBody StreamKeyRequestDto streamKey) {
-        // return streamService.checkStreamValidity(streamerId, streamKey);
-        return ResponseEntity.ok(false);
-    }
+	@PostMapping("/{streamerId}/check")
+	public Mono<Boolean> checkStreamValidity(@PathVariable String streamerId,
+		@RequestBody StreamKeyRequestDto streamKey) {
+		return streamService.checkStreamValidity(streamerId, streamKey);
+	}
 
-    @PostMapping("/{streamerId}/onair")
-    public ResponseEntity<String> startStream(@PathVariable String streamerId) {
-        log.info(streamerId + " 의 방송 시작 요청");
-        boolean started = streamService.startStream(streamerId);
-        if (started) {
-            return ResponseEntity.ok("방송이 시작됩니다. : " + streamerId);
-        } else {
-            return ResponseEntity.badRequest().body("방송 시작을 실패하였습니다. : " + streamerId);
-        }
-    }
+	@PostMapping("/{streamerId}/onair")
+	public Mono<Boolean> startStreamRequestFromRtmpServer(@PathVariable String streamerId) {
+		return streamService.startStream(streamerId);
+	}
 
-
-
-//    @PostMapping("/{streamName}/streaming")
-//    public ResponseEntity<String> stopStream(@RequestParam String streamName, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-//        boolean stopped = streamService.stopStream(streamName, userDetails.getUser());
-//        if (stopped) {
-//            return ResponseEntity.ok("스트리밍이 종료 되었습니다. : " + streamName);
-//        } else {
-//            return ResponseEntity.badRequest().body("스트리밍 종료가 실패하였습니다. : " + streamName);
-//        }
-//    }
-
-
+	@PostMapping("/{streamerId}/offair")
+	public Mono<Boolean> stopStreamRequestFromRtmpServer(@PathVariable String streamerId) {
+		return streamService.stopStream(streamerId);
+	}
 }
--- src/main/java/com/hanghae/lemonairservice/service/StreamService.java
+++ src/main/java/com/hanghae/lemonairservice/service/StreamService.java
@@ -1,9 +1,9 @@
 package com.hanghae.lemonairservice.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hanghae.lemonairservice.dto.stream.StreamKeyRequestDto;
-import com.hanghae.lemonairservice.entity.Member;
 import com.hanghae.lemonairservice.repository.MemberChannelRepository;
 import com.hanghae.lemonairservice.repository.MemberRepository;
 
@@ -28,23 +28,7 @@ public class StreamService {
 			.thenReturn(true);
 	}
 
-	public boolean startStream(String streamerId) {
-		memberRepository.findByLoginId(streamerId)
-			.flatMap(member -> memberChannelRepository.findByMemberId(member.getId()).doOnNext(memberChannel -> {
-				memberChannel.setOnAir(true);
-				memberChannelRepository.save(memberChannel).subscribe();
-			}))
-			.subscribe();
-		return true;
-	}
-
-	public boolean stopStream(String streamName, Member user) {
-		String streamKey = user.getStreamKey();
-		return false;
-	}
-
-	// TODO: 2023-12-19 Reactive Transactional 적용해아함
-	public Mono<Boolean> startStreamRequestFromRtmpServer(String streamerId) {
+	public Mono<Boolean> startStream(String streamerId) {
 		return memberRepository.findByLoginId(streamerId)
 			.switchIfEmpty(Mono.error(new RuntimeException("방송시작요청 멤버조회실패" + streamerId + " 는 가입되지 않은 아이디입니다.")))
 			.flatMap(member -> memberChannelRepository.findByMemberId(member.getId())
@@ -55,7 +39,7 @@ public class StreamService {
 				}));
 	}
 
-	public Mono<Boolean> stopStreamRequestFromRtmpServer(String streamerId) {
+	public Mono<Boolean> stopStream(String streamerId) {
 		return memberRepository.findByLoginId(streamerId)
 			.switchIfEmpty(Mono.error(new RuntimeException("방송종료 요청 멤버조회실패" + streamerId + " 는 가입되지 않은 아이디입니다.")))
 			.flatMap(member -> memberChannelRepository.findByMemberId(member.getId())
