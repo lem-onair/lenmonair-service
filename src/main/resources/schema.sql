5e2c4c1e8bf50581ee05b2569f8ac7f8979ca352
munee <121735319+alaneelee@users.noreply.github.com>
2023-12-22 01:37:55 +0900

    feat: 왜 되는거지?

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE member
 (
-    id         SERIAL PRIMARY KEY,
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     login_id     VARCHAR(255) NOT NULL UNIQUE,
@@ -10,9 +10,9 @@ CREATE TABLE member
 
 CREATE TABLE member_channel
 (
-    id                  SERIAL PRIMARY KEY,
+    id                  BIGINT PRIMARY KEY AUTO_INCREMENT,
     title               VARCHAR(255) NOT NULL UNIQUE,
-    member_id           VARCHAR(255) NOT NULL UNIQUE,
+    member_id           BIGINT NOT NULL UNIQUE,
     streamer_nickname   VARCHAR(255) NOT NULL,
     on_air              BOOLEAN NOT NULL
 );
