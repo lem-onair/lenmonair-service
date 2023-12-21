776e1cf059a3c3f8a6a2f089bc3b576cb8e5a01a
kangminbeom <alsqja6945@naver.com>
2023-12-21 09:05:25 +0900

    feat: 도네이션 기능 구현

--- src/main/java/com/hanghae/lemonairservice/controller/PointController.java
+++ src/main/java/com/hanghae/lemonairservice/controller/PointController.java
@@ -1,9 +1,24 @@
 package com.hanghae.lemonairservice.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hanghae.lemonairservice.dto.point.DonateRequestDto;
+import com.hanghae.lemonairservice.dto.point.DonateResponseDto;
+import com.hanghae.lemonairservice.service.PointService;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class PointController {
+	private final PointService pointService;
+
+	public Mono<ResponseEntity<DonateResponseDto>> donation(@RequestBody DonateRequestDto donateRequestDto){
+
+	}
 }
