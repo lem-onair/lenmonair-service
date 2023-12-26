c2bab069789a062993be6c494f26ce06e4e8e002
kangminbeom <alsqja6945@naver.com>
2023-12-27 02:31:09 +0900

    feature: 카카오 결제 기능과 관련된 문제 해결

--- src/main/java/com/hanghae/lemonairservice/controller/PaymentController.java
+++ src/main/java/com/hanghae/lemonairservice/controller/PaymentController.java
@@ -5,6 +5,8 @@ import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +20,21 @@ import jakarta.annotation.PostConstruct;
 public class PaymentController {
 	private IamportClient iamportClient;
 
+	@Value("${kakao.key}")
+	private String APIKey;
+	@Value("${kakao.Secret}")
+	private String APISecret;
+
 	@PostConstruct
 	public void PaymentController() {
-		this.iamportClient = new IamportClient("7485758722740306",
-			"18cJeOZQsnD3Zf9DGnnJ8NOMdXwrq3wROUBfFghmacXRO1BGYxijfuLIKQFe9Fa1Vk7ECDqPpLBIgs3z");
+		this.iamportClient = new IamportClient(APIKey,APISecret);
 	}
 
+	// imp_uid(거래 고유번호)를 검사하여, 데이터를 보내줍니다.
+	// payments.js에서 33~42번 줄의 코드가
+	// 1건의 결제 정보이고, 이 결제 정보의 고유번호가 imp_uid입니다.
+	// url의 경로를 통해 카카오페이 결제 기능을 제공하는 회사의 서버로 데이터를 전송하고
+	// 검사가 완료되면 카카오 결제 기능 창과 함께 데이터를 담아서 전송해줍니다.
 	@PostMapping("/verifyIamport/{imp_uid}")
 	public IamportResponse<Payment> paymentByImpUid(@PathVariable("imp_uid") String imp_uid)
 		throws IamportResponseException, IOException {
