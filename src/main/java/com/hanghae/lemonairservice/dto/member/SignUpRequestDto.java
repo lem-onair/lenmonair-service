91e592daf4c3fff294cd19ced5bcd4e2b8c84cdc
BYEONGRYEOL <lyulbyung@gmail.com>
2024-01-22 22:26:08 +0900

    Refactor: MemberService
    MemberService 클래스 리팩터링
    
    Ref: #1

--- src/main/java/com/hanghae/lemonairservice/dto/member/SignUpRequestDto.java
+++ src/main/java/com/hanghae/lemonairservice/dto/member/SignUpRequestDto.java
@@ -1,6 +1,7 @@
 package com.hanghae.lemonairservice.dto.member;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
@@ -9,6 +10,7 @@ public class SignUpRequestDto {
 	@Email
 	private String email;
 
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$")
 	private String password;
 
 	private String password2;
