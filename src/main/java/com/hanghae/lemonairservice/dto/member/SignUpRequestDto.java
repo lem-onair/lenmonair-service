f55366f36c326e13d458bdb703db311df12fbdb9
BYEONGRYEOL <lyulbyung@gmail.com>
2024-01-22 23:44:35 +0900

    Update: password valid check
    비즈니스 로직의 단순화를 위해 비밀번호의 유효성 검증을 Dto 생성시에 할 수 있도록 변경
    
    Ref: #1

--- src/main/java/com/hanghae/lemonairservice/dto/member/SignUpRequestDto.java
+++ src/main/java/com/hanghae/lemonairservice/dto/member/SignUpRequestDto.java
@@ -10,7 +10,7 @@ public class SignUpRequestDto {
 	@Email
 	private String email;
 
-	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$")
+	@Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,20}$", message = "비밀번호는 영문 대소문자 숫자 및 특수문자를 포함한 8~20자리 문자여야합니다.")
 	private String password;
 
 	private String password2;
